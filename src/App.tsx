704f2770b2c904c25718bcbf3f1aeafe556c1120
Sebastian0021 <sebastianalefuentespe@gmail.com>
2024-08-22 20:34:51 -0300

    Agregando SelectMenu al proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Chat from "./components/Chat/Chat";
+import SelectMenu from "./components/Chat/SelectMenu";
 
 export default function App() {
   return (
     <main className="flex flex-col justify-center items-center h-screen bg-slate-950">
+      <SelectMenu />
       <Chat />
     </main>
   );
