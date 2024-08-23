07aae07a56195acda2a67090ba62d32440111df5
Sebastian0021 <sebastianalefuentespe@gmail.com>
2024-08-23 18:15:55 -0300

    Agregando autor de la página (yo)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ export default function App() {
     <main className="flex flex-col justify-center items-center h-screen bg-slate-950">
       <SelectMenu />
       <Chat />
+      <span className="text-gray-500 absolute bottom-0">
+        Created by Sebastian Alejandro Peñaloza Fuentes
+      </span>
     </main>
   );
 }
